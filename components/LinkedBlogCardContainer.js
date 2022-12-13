536504b87bc129cb0bb0eebd6fafe21b846e7798
venkatesh-sdev <venkatesh.dev.main@gmail.com>
2022-12-13 14:54:29 +0530

    commit

--- components/LinkedBlogCardContainer.js
+++ components/LinkedBlogCardContainer.js
@@ -8,7 +8,7 @@ const LinkedBlogCardContainer = ({ posts, heading }) => {
       <h1 className="text-center text-gray-800 font-serif font-bold text-2xl sm:text-4xl my-5 items-center">
         {heading}
       </h1>
-      <div className="grid gap-5 grid-cols-4 justify-center">
+      <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center">
         {posts.map((post) => (
           <div key={post._id}>
             <BlogCard post={post} />
