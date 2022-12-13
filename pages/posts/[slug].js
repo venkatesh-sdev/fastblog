fcdef45c1753e76c80ec3e94fe39c7cfb45ded5e
venkatesh-sdev <venkatesh.dev.main@gmail.com>
2022-12-13 06:56:49 +0530

    slugData Fetched

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { sanityClient } from '../../santiy';
 
-const PostDetails = ({value}) => {
+const PostDetails = ({post}) => {
+  console.log(post);
   return (
     <div>
-    post-{value}
+    
     </div>
   )
 }
@@ -14,9 +15,27 @@ export default PostDetails
 
 
 export const getServerSideProps = async ({query}) => {
+
+  const queryFetch = `*[_type=="post" && slug.current == $slug][0]{
+    _id,
+      title,
+      slug,
+      author->{
+        name,
+        image
+      },
+      mainImage,
+      publishedAt,
+      body
+  }`;
+
+  const post = await sanityClient.fetch(queryFetch,{
+    slug:query?.slug
+  })
+
   return{
     props:{
-      value:query?.slug,
+      post,
     }
   }
 };
